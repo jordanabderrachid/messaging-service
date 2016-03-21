38ad9bd37f8b028107598831f4f0068d4871b7a4
Jordan Abderrachid <jabderrachid@gmail.com>
2016-03-21 14:09:12 +0100

    Fix concurrency acces to websockets map.

--- messaging.go
+++ messaging.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
 
 type messaging struct {
+	lk      sync.Mutex
 	sockets map[int]*websocket.Conn
 }
 
@@ -18,13 +20,17 @@ func (m *messaging) register(ws *websocket.Conn) int {
 
 	id := rand.Int()
 	fmt.Println("registering ", id)
+	m.lk.Lock()
 	m.sockets[id] = ws
+	m.lk.Unlock()
 	return id
 }
 
 func (m *messaging) unregister(id int) {
 	fmt.Println("unregistering ", id)
+	m.lk.Lock()
 	delete(m.sockets, id)
+	m.lk.Unlock()
 }
 
 func (m *messaging) sendMessage(mess message) error {
